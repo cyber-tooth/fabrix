d5e1fa3f7ba3988c153fcdf65a0e6adf8cfd36df
haticeca <cakmakha@outlook.de>
2020-10-29 14:52:22 +0100

    neue Komponente

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AboutComponent} from './about/about.component';
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
