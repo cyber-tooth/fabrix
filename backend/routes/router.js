ac5c6e203bde6fcce99c617707d18f6e101a2f8d
laurawrsh <65012656+laurawrsh@users.noreply.github.com>
2020-11-05 12:38:17 +0100

    Kommentare hinzugefügt

---
+++ backend/routes/router.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
+
+const db = require('../lib/db.js');
+const userMiddleware = require('../middleware/users.js');
+
+//Fehlermeldung bei query, escape, hash
+// Vielleicht findest du den Fehler. Ich habe noch keine Datenbank erstellt.
+// Vielleicht liegt es daran.
+router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
+    db.query(
+        `SELECT * FROM users WHERE LOWER (username) = LOWER (${db.escape(
+            req.body.username
+        )});`,
+        (err, result) => {
+            if (result.length) {
+                return res.status(409).send({
+                    msg: 'This username is already in use!'
+                });
+            } else {
+                // username is available
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).send({
+                            msg: err
+                        });
+                    } else {
+                        // has hashed pw => add to database
+                        db.query(
+                            `INSERT INTO users (id, username, password, registered) VALUES ('${uuid.v4()}', ${db.escape(
+                                req.body.username
+                            )}, ${db.escape(hash)}, now())`,
+                            (err, result) => {
+                                if (err) {
+                                    throw err;
+                                    return res.status(400).send({
+                                        msg: err
+                                    });
+                                }
+                                return res.status(201).send({
+                                    msg: 'Registered!'
+                                });
+                            }
+                        );
+                    }
+                });
+            }
+        }
+    )
+});
+
+router.post('/login', (req, res, next) => {});
+
+router.get('/secret-route', (req, res, next) => {
+    res.send('This is the secret content. Only logged in users can see that!');
+});
+
+module.exports = router;
