a00b60e533e603b44937bc454aadb80e9d9270ab
laurawrsh <65012656+laurawrsh@users.noreply.github.com>
2020-11-05 12:05:26 +0100

    angefangen mit Login

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,19 @@
 const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//set up port
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//add routes
+const router = require('./routes/router.js');
+app.use('/api', router);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const server = http.createServer(function(request, response) {
     response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8'});
