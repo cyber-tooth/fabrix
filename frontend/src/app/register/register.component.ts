6c7029429518e6f2f744012fc0df2ae01393fbfe
haticeca <cakmakha@outlook.de>
2020-11-17 22:01:44 +0100

    aktuell

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
   person: {
     firstname;
     lastname;
-   
+
     email;
     id;
   }
