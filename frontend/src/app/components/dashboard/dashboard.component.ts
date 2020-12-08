581f14697745bf11b3f70500bacb072ddff16813
haticeca <cakmakha@outlook.de>
2020-12-08 14:42:10 +0100

    forgot erweitert, dashboard erweitert

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService, AuthorisationService} from "../../services";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
   text = 'Additional Info-Text on our Info Box! 🎊';
   hidden = true;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService,
+              private authorisationService: AuthorisationService) {}
 
-  ngOnInit() {}
+    ngOnInit()
+    {
+    }
+
+    getFirstName()
+    {
+      if (JSON.parse(localStorage.getItem('currentUser'))) {
+        return JSON.parse(localStorage.getItem('currentUser')).firstName;
+      } else {
+        return '';
+      }
+    }
+
+    hasAccess(roles)
+    {
+      return this.authorisationService.hasAccess(roles);
+    }
+
 
 }
