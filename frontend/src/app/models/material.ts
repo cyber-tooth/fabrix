d61f5889ce7843f4a492c355a1bd83ebb506819f
Unknown <unknown@example.com>
2021-01-27 18:06:02 +0100

    model material erweitert

--- frontend/src/app/models/material.ts
+++ frontend/src/app/models/material.ts
@@ -2,172 +2,46 @@ export class Material {
   id: string;
   name: string;
   CREATED_BY: string;
-  categories: [
-      mainCategories: [
-        CATEGORY_ID: string,
-        categoryName: string,
-        degree: string,
-        materialComposition: [
-          materialCompositionName: string,
-          naturalMaterial: string,
+  mainCategory: [
+    CATEGORY_ID: string,
+    categoryName: string,
+    degree: string,
+    child: {
+      materialComposition: {
+        materialCompositionName: string,
+        child: {
+          naturalMaterial: {
+            cotton: string,
+            cashmere: string,
+            wool: string
+          },
           syntheticFibre: string,
-          other: string
-        ],
-        threeDProgramm: string,
-        thickness: string,
-        surfaceLook: [
-          weave: string,
-          knit: string,
-          nonWoven: string
-        ],
-        weight: string,
-        productGroup: string[],
-        subCategories: [
-          opacity: string,
-          drapeCoefficient: string,
-          commercialFabricName: string
-        ]
-      ],
-      subCategories: [
-        opacity: string,
-        drapeCoefficient: string,
-        commercialFabricName: string,
-        elasticity: [
-          totalElongationMean: [
-            longitudinal: string,
-            cross: string
-          ],
-          remainingAfterRemoval: [
-            longitudinal: string,
-            cross: string
-          ],
-          remainingElongationAfterRecovery: [
-            longitudinal: string,
-            cross: string
-          ]
-        ],
-        elongation: [
-          fh_n_chain: string,
-          fh_n_shots: string
-        ]
-      ],
-    additionalInfo: [
-      abrasionResistance: string,
-      burstPressure: string,
-      careInstructions: string,
-      density: string,
-      electricalCharge: string,
-      finishing: string,
-      pilling: string,
-      shrinkage: string,
-      sustainability: string,
-      tensileStrength: string,
-      offerer: string,
-      numberThreadsUnitLength: [
-        threads: string,
-        weftThreads: string,
-        warpThreads: string
-      ],
-      stitchesPerUnitLength: [
-        meshes: string,
-        wales: string,
-        rowsStitches: string
-      ]
-    ]
-  ];
-  pictures: [
-    url: string,
-    title: string
-  ];
+          other: {
+            transparent: string;
+            semiTrans: string;
+            opaque: string;
+          }
+        }
+      },
+      weight: string,
+      elasticity: {
+        gesamtDehnungMittelwert: string
+      }
+    ];
+  subCategory: [
+      CATEGORY_ID: string,
+      categoryName: string,
+      degree: string,
+      child: {
+        opacity: string
+      }
+    ];
 
-  constructor(id: string, name: string,
-              CREATED_BY: string,
-              categories: [
-                mainCategories: [
-                  CATEGORY_ID: string,
-                  categoryName: string,
-                  degree: string,
-                  materialComposition: [
-                    materialCompositionName: string,
-                    naturalMaterial: string,
-                    syntheticFibre: string,
-                    other: string
-                  ],
-                  threeDProgramm: string,
-                  thickness: string,
-                  surfaceLook: [
-                    weave: string,
-                    knit: string,
-                    nonWoven: string
-                  ],
-                  weight: string,
-                  productGroup: string[],
-                  subCategories: [
-                    opacity: string,
-                    drapeCoefficient: string,
-                    commercialFabricName: string
-                  ]
-                ],
-                subCategories: [
-                  opacity: string,
-                  drapeCoefficient: string,
-                  commercialFabricName: string,
-                  elasticity: [
-                    totalElongationMean: [
-                      longitudinal: string,
-                      cross: string
-                    ],
-                    remainingAfterRemoval: [
-                      longitudinal: string,
-                      cross: string
-                    ],
-                    remainingElongationAfterRecovery: [
-                      longitudinal: string,
-                      cross: string
-                    ]
-                  ],
-                  elongation: [
-                    fh_n_chain: string,
-                    fh_n_shots: string
-                  ]
-                ],
-                additionalInfo: [
-                  abrasionResistance: string,
-                  burstPressure: string,
-                  careInstructions: string,
-                  density: string,
-                  electricalCharge: string,
-                  finishing: string,
-                  pilling: string,
-                  shrinkage: string,
-                  sustainability: string,
-                  tensileStrength: string,
-                  offerer: string,
-                  numberThreadsUnitLength: [
-                    threads: string,
-                    weftThreads: string,
-                    warpThreads: string
-                  ],
-                  stitchesPerUnitLength: [
-                    meshes: string,
-                    wales: string,
-                    rowsStitches: string
-                  ]
-                ]
-              ],
-              pictures: [
-                url: string,
-                title: string
-              ])
-  {
-    this.id = id;
-    this.name = name;
-    this.CREATED_BY = CREATED_BY;
-    this.categories = categories;
-    this.pictures = pictures;
+  constructor( ) {
+
   }
 }
-// tslint:disable-next-line:no-namespace
+
 export namespace Material {
 }
 
