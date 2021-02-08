b6d0406b25aef3657778010f7842a2393bc84429
janoszczyk <paulina.janoszczyk@student.htw-berlin.de>
2021-02-09 00:36:41 +0100

    // TODO Laura Children should be given as Array, Frontend cannot visualise the children as Object --> getCategoryTreeById

--- frontend/src/app/components/edit-material/edit-material.component.ts
+++ frontend/src/app/components/edit-material/edit-material.component.ts
@@ -76,7 +76,7 @@ export class EditMaterialComponent implements OnInit {
 
   // hier noch material.name nach hinzugefügte person ändern..
   adminAdded(material: Material): boolean {
-    if (material.created_by === 'RoleEnum.admin')
+    if (material.createdBy === 'RoleEnum.admin')
     {
       return true;
     }
