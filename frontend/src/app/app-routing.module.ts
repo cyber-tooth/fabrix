4036c74b41c67e63d58e100d5ee93c6eac336fa6
janoszczyk <paulina.janoszczyk@student.htw-berlin.de>
2021-02-09 22:05:46 +0100

    clean up FE

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -19,10 +19,6 @@ import {DatenschutzComponent} from './components/datenschutz/datenschutz.compone
 import {FaqComponent} from './components/faq/faq.component';
 import {AuthGuardSuperAdmin, AuthGuardUser, AuthGuardAdmin} from './guards/index';
 import {AddMaterialComponent} from './components/add-material/add-material.component';
-import {FilterCardComponent} from './components/filter-card/filter-card.component';
-import {MaterialCardComponent} from './components/material-card/material-card.component';
-import {MaterialcardListComponent} from './components/materialcard-list/materialcard-list.component';
-import {RoleDropdownComponent} from "./components/role-dropdown/role-dropdown.component";
 
 
 const routes: Routes = [
@@ -45,10 +41,6 @@ const routes: Routes = [
     canActivate: [AuthGuardSuperAdmin]},
   {path: 'edit-material', component: EditMaterialComponent,
     canActivate: [AuthGuardAdmin]},
-  {path: 'materialcard-list', component: MaterialcardListComponent},
-  {path: 'materialCard/:id', component: MaterialCardComponent},
-  {path: 'materialCard', component: MaterialCardComponent},
-  {path: 'filter-card', component: FilterCardComponent},
   {path: 'add-material', component: AddMaterialComponent,
     canActivate: [AuthGuardSuperAdmin]
   }
